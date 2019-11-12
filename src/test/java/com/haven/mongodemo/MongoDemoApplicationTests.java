51fb3d947604b742b27c12e186d4731dae6b12e7
HavenTong <852092786@qq.com>
2019-11-12 16:49:32 +0800

    Initialize the project

---
+++ src/test/java/com/haven/mongodemo/MongoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haven.mongodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
